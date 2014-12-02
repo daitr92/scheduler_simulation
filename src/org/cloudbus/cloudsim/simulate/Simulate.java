950fa971461303b4a1f93c06202fe4fdb7b09c30
Dai Tran <dai.tr@geekup.vn>
2014-12-02 07:40:55 +0700

    Read JSON file

--- src/org/cloudbus/cloudsim/simulate/Simulate.java
+++ src/org/cloudbus/cloudsim/simulate/Simulate.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.CloudletSchedulerSpaceShared;
-import org.cloudbus.cloudsim.Datacenter;
 import org.cloudbus.cloudsim.DatacenterBroker;
 import org.cloudbus.cloudsim.DatacenterCharacteristics;
 import org.cloudbus.cloudsim.Host;
@@ -76,53 +75,39 @@ public class Simulate {
             	List<Vm> vmList = new ArrayList<Vm>();
             	List<Cloudlet> cloudletList = new ArrayList<Cloudlet>();
             	
-            	// Cretae datacenters
-            	JSONArray m_datacenters = (JSONArray) member.get("datacenters");
-            	for (int j = 0; j < m_datacenters.size(); j++) {
-            		JSONObject m_datacenter = (JSONObject) m_datacenters.get(j);
-            		Datacenter datacenter = createDatacenter(m_datacenter, broker);
-            		
-            		// Create vms
-                	JSONArray m_vms = (JSONArray) m_datacenter.get("vms");
-                	if (m_vms != null) {
-    	            	for (int k = 0; k < m_vms.size(); k++) {
-    	            		JSONObject m_vm = (JSONObject) m_vms.get(k);
-    	            		Vm vm = createVirtualMachine(vmList.size(), m_vm, broker);
-    	            		vmList.add(vm);
-    	            	}
-                	}
-            	}
+            	// Create datacenters
+            	JSONObject m_datacenters = (JSONObject) member.get("datacenters");
+            	createDatacenter(m_datacenters, broker);
+            	
+            	JSONObject m_vm = (JSONObject) m_datacenters.get("vms");
+            	createVm(vmList, m_vm, broker);
             	broker.submitVmList(vmList);
             	
-            	JSONArray m_cloudlets = (JSONArray) member.get("cloudlets");
-            	if (m_cloudlets == null) {
+            	JSONObject m_cloudlet = (JSONObject) member.get("cloudlets");
+            	if (m_cloudlet == null) {
             		Log.printLine(broker.getName() + ": There is no cloudlet");
             		continue;
             	}
             	
-            	int totalVms = vmList.size();
-            	if (totalVms <= 0) {
-            		Log.printLine(broker.getName() + ": There is no vm => can not submit cloudlet.");
-            		continue;
-            	}
+            	int cloudlet_quantity = ((Long) m_cloudlet.get("quantity")).intValue();
+            	int cloudletId_prefix = broker.getId() * 1000;
             	
-            	for (int j = 0; j < m_cloudlets.size(); j++) {
-            		int selectedVm = j % totalVms;
-            		JSONObject m_cloudlet = (JSONObject) m_cloudlets.get(j);
-            		long length = (Long) m_cloudlet.get("long");
-            		long fileSize = (Long) m_cloudlet.get("fileSize");
-            		long outputSize = (Long) m_cloudlet.get("outputSize");
-            		int pesNumber = ((Long) m_cloudlet.get("pesNumber")).intValue();
-            		
+        		long length = (Long) m_cloudlet.get("long");
+        		long fileSize = (Long) m_cloudlet.get("fileSize");
+        		long outputSize = (Long) m_cloudlet.get("outputSize");
+        		int pesNumber = ((Long) m_cloudlet.get("pesNumber")).intValue();
+        		double deadline = (Double) m_cloudlet.get("deadline");
+            	
+            	for (int j = 0; j < cloudlet_quantity; j++) {          		
             		UtilizationModel utilizationModel = new UtilizationModelFull();
             		
-            		Cloudlet cloudlet = new Cloudlet(j, length, pesNumber, fileSize, outputSize, 
+            		Cloudlet cloudlet = new Cloudlet(cloudletId_prefix + j, length, pesNumber, fileSize, outputSize, 
             				utilizationModel, utilizationModel, utilizationModel);
-            		
             		cloudlet.setUserId(broker.getId());
-            		cloudlet.setVmId(selectedVm);
-            		
             		cloudletList.add(cloudlet);
+            		
+            		Log.printLine("Cloudlet #" + cloudlet.getCloudletId() 
+            				+ " has been created and is belonged to Broker #" + cloudlet.getUserId());
             	}
 
             	broker.submitCloudletList(cloudletList);
@@ -144,40 +129,27 @@ public class Simulate {
 		}
 	}
 	
-	private static Vm createVirtualMachine(int vmId, JSONObject v_info, DatacenterBroker broker) {
-		int mips = ((Long) v_info.get("mips")).intValue();
-		long size = (Long) v_info.get("size"); // image size (MB)
-		int ram = ((Long) v_info.get("ram")).intValue(); // vm memory (MB)
-		long bw = (Long) v_info.get("bw");
-		int pesNumber = ((Long) v_info.get("pesNumber")).intValue(); // number of cpus
-		String vmm = "Xen"; // VMM name
-
-		// create VM
-		Vm vm = new Vm(vmId, broker.getId(), mips, pesNumber, ram, bw, size, vmm,
-				new CloudletSchedulerSpaceShared());
+	private static void createDatacenter(JSONObject d_info, CustomDatacenterBroker broker) {
+		String name = broker.getName();
+		Log.printLine(name + ": creating datacenters");
 		
-		return vm;
-	}
-	
-	private static Datacenter createDatacenter(JSONObject d_info, CustomDatacenterBroker broker) {
-		String name = broker.getName() + "---" + (String) d_info.get("name");
-		Log.printLine(name);
+		int datacenter_quantity = ((Long) d_info.get("quantity")).intValue();
 		
 		List<Host> hostList = new ArrayList<Host>();
 		
-		JSONArray hosts = (JSONArray) d_info.get("hosts");
-		for (int i = 0; i < hosts.size(); i++) {
-			JSONObject host = (JSONObject) hosts.get(i);
+		JSONObject hosts = (JSONObject) d_info.get("hosts");
+		int hosts_quantity = ((Long) hosts.get("quantity")).intValue();
+		for (int i = 0; i < hosts_quantity; i++) {
+			int ram = ((Long) hosts.get("ram")).intValue();
+			long storage = (Long) hosts.get("storage");
+			int bw = ((Long) hosts.get("bw")).intValue();
 			
-			int ram = ((Long) host.get("ram")).intValue();
-			long storage = (Long) host.get("storage");
-			int bw = ((Long) host.get("bw")).intValue();
-			
-			JSONArray pes = (JSONArray) host.get("pes");
+			JSONObject pes = (JSONObject) hosts.get("pes");
+			int pes_quantity = ((Long) pes.get("quantity")).intValue();
+			long pes_mips = (Long) pes.get("mips");
 			List<Pe> peList = new ArrayList<Pe>();
-			for (int j = 0; j < pes.size(); j++) {
-				long mips = (Long) pes.get(j);
-				peList.add(new Pe(j, new PeProvisionerSimple(mips)));
+			for (int j = 0; j < pes_quantity; j++) {
+				peList.add(new Pe(j, new PeProvisionerSimple(pes_mips)));
 			}
 			
 			hostList.add(new Host(i, new RamProvisionerSimple(ram), new BwProvisionerSimple(bw), 
@@ -199,18 +171,43 @@ public class Simulate {
 				arch, os, vmm, hostList, time_zone, cost, costPerMem,
 				costPerStorage, costPerBw);
 
-		// 6. Finally, we need to create a PowerDatacenter object.
-		Datacenter datacenter = null;
-		try {
-			datacenter = new CustomDatacenter(name, characteristics,
-					new VmAllocationPolicySimple(hostList), storageList, 0, broker);
-			
-			broker.addDatacenter(datacenter.getId());
-		} catch (Exception e) {
-			e.printStackTrace();
+		// 6. Finally, we need to create a Datacenter object.
+		for (int i = 0; i < datacenter_quantity; i++) {
+			try {
+				CustomDatacenter datacenter = new CustomDatacenter(name, characteristics,
+						new VmAllocationPolicySimple(hostList), storageList, 0, broker);
+				
+				broker.addDatacenter(datacenter.getId());
+				
+				Log.printLine("Datacenter #" + datacenter.getId() 
+						+ " has been created and is belonged to Broker #" + datacenter.getDatacenterBrokerId());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+	}
+	
+	private static void createVm(List<Vm> vmList, JSONObject m_vm, CustomDatacenterBroker broker) {
+		String brokerName = broker.getName();
+		int vm_quantity = ((Long) m_vm.get("quantity")).intValue();
+		int vmId_prefix = broker.getId() * 1000;
+		for (int i = 0; i < vm_quantity; i++) {
+			int mips = ((Long) m_vm.get("mips")).intValue();
+			long size = (Long) m_vm.get("size"); // image size (MB)
+			int ram = ((Long) m_vm.get("ram")).intValue(); // vm memory (MB)
+			long bw = (Long) m_vm.get("bw");
+			int pesNumber = ((Long) m_vm.get("pesNumber")).intValue(); // number of cpus
+			String vmm = "Xen"; // VMM name
+			int vmId = vmId_prefix + i;
 
-		return datacenter;
+			// create VM
+			Vm vm = new Vm(vmId, broker.getId(), mips, pesNumber, ram, bw, size, vmm,
+					new CloudletSchedulerSpaceShared());
+			
+			vmList.add(vm);
+
+			Log.printLine(brokerName + ": creating Vm #" + vmId);
+		}
 	}
 
 	
